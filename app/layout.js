0a35e3cd7a0b98597a1ab5ea64decedfe7ff0bb7
Vital-pro <vitupikov@yandex.ru>
2024-07-20 19:39:34 +0300

    added span and modified time tags

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         <main className='main container'>
           <div className='geolocation-wrapper'>
             <h1>Крым г.Судак</h1>
-            <time dateTime={date}>
+            <time dateTime={date.getHours() + ':' + date.getMinutes()}>
               Сегодня:{' '}
               {date.toLocaleString('ru-RU', { month: 'long', day: 'numeric' })}{' '}
               {date.getFullYear()} г.
