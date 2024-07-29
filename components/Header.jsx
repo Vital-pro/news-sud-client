e53ab36e22e8523f3ad32cdd8f2099304846d730
Vital-pro <vitupikov@yandex.ru>
2024-07-29 23:58:07 +0300

    added changed background with time

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,23 @@
-import Link from 'next/link'
+import Link from 'next/link';
+
+let date = new Date();
+let dateHour = date.getHours() + 3;
 
 export const Header = () => {
   return (
-    <header className='header'>
+    <header
+      className='header'
+      style={{
+        borderImage:
+          dateHour >= 4 && dateHour < 17
+            ? 'fill 0 linear-gradient(190deg, #0000, #0000)'
+            : dateHour >= 17 && dateHour <= 21
+            ? 'fill 0 linear-gradient(190deg, #0004, #0004)'
+            : dateHour > 21
+            ? 'fill 0 linear-gradient(190deg, #0009, #0009)'
+            : '',
+      }}
+    >
       <div className='header__container container'>
         <p className='header__title'>Солнечные новости</p>
       </div>
@@ -13,4 +28,4 @@ export const Header = () => {
       </nav>
     </header>
   );
-}
+};
